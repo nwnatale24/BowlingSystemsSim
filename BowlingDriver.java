beea59b28560a6b77743a86bf63032b43d919b4b
Nicholas Natale <nwnatale24@gmail.com>
2023-04-26 17:24:41 -0400

    added basic scanner to get the number of games to simulate from user.

--- BowlingDriver.java
+++ BowlingDriver.java
@@ -1,9 +1,28 @@
+// 4/26/23
+//
+// Systems Simulation and Modeling - Fall 2023
+//
+// Nicholas Natale
+// Daniel Ortiz
+// Nikolas Vasiliadis
+// Luck Heck
+
+import java.util.Scanner;
+
 
 public class BowlingDriver {
     public static void main(String []args) {
-        
-        BowlingSim bowlingSim = new BowlingSim();
-        System.out.println(bowlingSim.test);
+
+        System.out.println("Enter Number of Bowling Games to Simulate: ");
+        Scanner scanner = new Scanner(System.in);
+
+        String numGames = scanner.nextLine();
+        scanner.close(); 
+        System.out.println("Number of Games Entered: " + numGames);
+
+
+        //BowlingSim bowlingSim = new BowlingSim();
+        //System.out.println(bowlingSim.test);
 
 
     }
--- BowlingSim.java
+++ BowlingSim.java
@@ -1,3 +1,12 @@
+// 4/26/23
+//
+// Systems Simulation and Modeling - Fall 2023
+//
+// Nicholas Natale
+// Daniel Ortiz
+// Nikolas Vasiliadis
+// Luck Heck
+
 public class BowlingSim {
     
     public int test = 5;
