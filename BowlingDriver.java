205d60d71e41588dda40ae76f712e41cb599bac2
Nicholas Natale <nwnatale24@gmail.com>
2023-04-26 17:13:22 -0400

    initial commit. adding basic backbone files for simulation.

---
+++ BowlingDriver.java
@@ -0,0 +1,10 @@
+
+public class BowlingDriver {
+    public static void main(String []args) {
+        
+        BowlingSim bowlingSim = new BowlingSim();
+        System.out.println(bowlingSim.test);
+
+
+    }
+}
---
+++ BowlingSim.java
@@ -0,0 +1,10 @@
+public class BowlingSim {
+    
+    public int test = 5;
+
+    public BowlingSim() {
+        test = 12;
+    }
+
+   
+}
