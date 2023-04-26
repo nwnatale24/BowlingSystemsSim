82862ede61287aa81457c2c3d3a7d76ad7995da2
nikvas06 <nikvas99@gmail.com>
2023-04-26 19:27:58 -0400

    Bowling Game class and Simulation template

---
+++ BowlingGame.java
@@ -0,0 +1,66 @@
+public class BowlingGame{
+    public int strikes;
+    public int spares;
+    public int gutter_balls;
+    public int rental_shoes;
+    public int split_pins;
+    public int misses;
+    public int no_spares;
+    public int machine_failures;
+
+    public BowlingGame(int strikes, int spares, int gutter_balls, int rental_shoes, int split_pins, int misses, int no_spares, int machine_failures){
+        this.strikes = strikes;
+        this.spares = spares;
+        this.gutter_balls = gutter_balls;
+        this.rental_shoes = rental_shoes;
+        this.split_pins = split_pins;
+        this.misses = misses;
+        this.no_spares = no_spares;
+        this.machine_failures = machine_failures;
+    }
+
+    public int getStrikes(){
+        return strikes;
+    }
+
+    public int getSpares(){
+        return spares;
+    }
+
+    public int getGutters(){
+        return gutter_balls;
+    }
+
+    public int getRentals(){
+        return rental_shoes;
+    }
+
+    public int getSplits(){
+        return split_pins;
+    }
+
+    public int getMisses(){
+        return misses;
+    }
+
+    public int getNoSpares(){
+        return no_spares;
+    }
+
+    public int getMachineFailures(){
+        return machine_failures;
+    }
+
+    public String toString(){
+        return (
+        "Strikes: " + strikes +
+        "Spares: " + spares +
+        "Gutter Balls: " + gutter_balls +
+        "Rental Shoes: " + rental_shoes + 
+        "Splits: " + split_pins +
+        "Misses: " + misses +
+        "Turns with no spares: " + no_spares +
+        "Mechanical Failures: " + machine_failures);
+    }
+
+}
--- BowlingSim.java
+++ BowlingSim.java
@@ -1,10 +1,90 @@
+import BowlingGame;
+import java.util.Random;
+import java.util.ArrayList;
+import java.lang.Math;
+
+
 public class BowlingSim {
     
-    public int test = 5;
+    Random random = new Random();
+    int numOfGames = 10;
+    public ArrayList<BowlingGame> gameQueue = new ArrayList<BowlingGame>(numOfGames);
+
+    //Min/Max Values for Simulation
+    private strikeMin = 0;
+    private StrikeMax = 10;
+    private spareMin = 0;
+    priavte spareMax = 10;
 
     public BowlingSim() {
-        test = 12;
+        //Game Queue with one 'dummy' game placeholder if no games are given
+        gameQueue[0] = new BowlingGame(0, 0, 0, 0, 0, 0, 0, 0);
     }
+
+    //Main Constructor for Driver Program
+    public BowlingSim(int numOfGames) {
+        this.numOfGames = numOfGames;
+
+    }
+
+
+    public ArrayList<BowlingGame> getGameQueue(){
+        return gameQueue;
+    }
+
+
+
+
+    public createGames(int numOfGames){
+        for (int i = 0; i < gameQueue.length(); i++){
+            gameQueue.add(
+                new BowlingGame(
+                    strikeData(),
+                    spareData(), 
+                    gutterData(), 
+                    rentalData(), 
+                    splitData(), 
+                    missData(), 
+                    noSpareData(), 
+                    machineFailureData()));
+        }
+    }
+
+
+    //Logic for inputting random variables into simulation
+    
+    public int strikeData(){
+        return ((random.nextInt()));
+    }
+
+    public int spareData(){
+        return ((random.nextInt()));
+    }
+
+    public int gutterData(){
+        return ((random.nextInt()));
+    }
+
+    public int rentalData(){    
+        return ((random.nextInt()));
+    }
+
+    public int splitData(){
+        return ((random.nextInt()));
+    }
+
+    public int missData(){
+        return ((random.nextInt()));
+    }
+
+    public int noSpareData(){
+        return ((random.nextInt()));
+    }
+
+    public int machineFailureData(){
+        return ((random.nextInt()));
+    }
+
 
    
 }
