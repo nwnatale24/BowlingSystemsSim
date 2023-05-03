b97203ad740eaf45f5bd32c5a3aaef9bac5eb1ac
ortizd16 <114445687+ortizd16@users.noreply.github.com>
2023-05-03 09:35:12 -0400

    Dano testing (#1)
    * Adding temp work
    
    * Update to previous commit with more changes and missing files

--- BowlingSim.java
+++ BowlingSim.java
@@ -1,7 +1,6 @@
 
 import java.util.Random;
 import java.util.ArrayList;
-import java.lang.Math;
 
 
 public class BowlingSim {
@@ -13,9 +12,12 @@ public class BowlingSim {
     public ArrayList<Bowler> playerQueue = new ArrayList<Bowler>(numOfPlayers);
 
     //debugging variables
-    int zeroToOne = 0;
-    int twoToFive = 0;
-    int sixToNine = 0;
+    int totalStrikes=0;
+    int totalSpares=0;
+    int totalGB=0;
+    int totalSplit=0;
+    int totalMiss=0;
+    int totalNoSpares=0;
     
 
 
@@ -62,186 +64,173 @@ public class BowlingSim {
 
         //from Pseudocode Dan O. wrote in class
         // 0.0 - 0.22: 0-1 Strikes
-        // 0.22 - 0.63: 2-5 Strikes
-        // 0.63 - 1.0: 6-9 Strikes
+        // 0.22 - 0.90: 2-5 Strikes
+        // 0.9 - 1.0: 6-9 Strikes
 
         
 
-        int r = random.nextInt(numOfPlayers);
-        int r2 = 0;
+        int r = random.nextInt(0,101); //Simulates a random number from 0-100 for determining how many strikes
+        int r2 = 0; //This variable will determine the actual number of stikes onces the proper range is determined
 
-        if(r <= 22){
-            r2 = random.nextInt(2);
-            zeroToOne++;
-        } else if (r > 22 && r <= 63){
-            int result = random.nextInt(6);
-		        while(result < 2 || result >= 6) {
-			        result = random.nextInt(6);
-		        }
-            r2 = result;
-            twoToFive++;
-            
-        } else {
-            int result = random.nextInt(9);
-		        while(result < 6 || result >= 9) {
-			        result = random.nextInt(9);
-		        }
-            r2 = result;
-            sixToNine++;
-
+        if(r <= 22){ 
+            r2 = random.nextInt(0,2); 
         }
-        
-        return (r2);
+         else if (r > 22 && r <= 68){
+            r2 = random.nextInt(2,4);
+        }  
+        else if (r > 68 && r <= 93){
+             r2 = random.nextInt(4,6);
+        }
+         else {
+            r2 = random.nextInt(6,9);
+            
+        }
+        totalStrikes += r2;
+        return (r2); //Returns the value of number of strikes for a particular bowler in 10 frames
     }
 
     public int spareData(){
 
-        //if statements need to have changed values for data
-        int r = random.nextInt(numOfPlayers);
-        int r2 = 0;
+        //from Pseudocode Dan O. wrote in class
+        // 0.0 - 0.22: 0-1 Strikes
+        // 0.22 - 0.90: 2-5 Strikes
+        // 0.9 - 1.0: 6-9 Strikes
 
-        if(r <= 22){
-            r2 = random.nextInt(2);
-            zeroToOne++;
-        } else if (r > 22 && r <= 63){
-            int result = random.nextInt(6);
-		        while(result < 2 || result >= 6) {
-			        result = random.nextInt(6);
-		        }
-            r2 = result;
-            twoToFive++;
-            
-        } else {
-            int result = random.nextInt(9);
-		        while(result < 6 || result >= 9) {
-			        result = random.nextInt(9);
-		        }
-            r2 = result;
-            sixToNine++;
-
-        }
         
-        return (r2);
+
+        int r = random.nextInt(0,101); //Simulates a random number from 0-100 for determining how many strikes
+        int r2 = 0; //This variable will determine the actual number of stikes onces the proper range is determined
+
+        if(r <= 24){ 
+            r2 = random.nextInt(0,2);
+        }
+         else if (r > 24 && r <= 70){
+            r2 = random.nextInt(2,4);
+        }  
+        else if (r > 70 && r <= 94){
+             r2 = random.nextInt(4,6);
+        }
+         else {
+            r2 = random.nextInt(6,9);
+        }
+        totalSpares += r2;
+        return (r2); //Returns the value of number of strikes for a particular bowler in 10 frames
     }
 
     public int gutterData(){
-        
-        //if statements need to have changed values for data
-        int r = random.nextInt(numOfPlayers);
-        int r2 = 0;
 
-        if(r <= 22){
-            r2 = random.nextInt(2);
-            zeroToOne++;
-        } else if (r > 22 && r <= 63){
-            int result = random.nextInt(6);
-		        while(result < 2 || result >= 6) {
-			        result = random.nextInt(6);
-		        }
-            r2 = result;
-            twoToFive++;
-            
-        } else {
-            int result = random.nextInt(9);
-		        while(result < 6 || result >= 9) {
-			        result = random.nextInt(9);
-		        }
-            r2 = result;
-            sixToNine++;
+        //from Pseudocode Dan O. wrote in class
+        // 0.0 - 0.22: 0-1 Strikes
+        // 0.22 - 0.90: 2-5 Strikes
+        // 0.9 - 1.0: 6-9 Strikes
+
+        
+
+        int r = random.nextInt(0,101); //Simulates a random number from 0-100 for determining how many strikes
+        int r2 = 0; //This variable will determine the actual number of stikes onces the proper range is determined
+
+        if(r <= 23){ 
+            r2 = random.nextInt(0,2);
+        }
+         else if (r > 23 && r <= 70){
+            r2 = random.nextInt(2,4);
+        }  
+        else if (r > 70 && r <= 94){
+             r2 = random.nextInt(4,6);
 
         }
-        
-        return (r2);
+         else {
+            r2 = random.nextInt(6,9);
+        }
+        totalGB += r2;
+        return (r2); //Returns the value of number of strikes for a particular bowler in 10 frames
     }
 
     public int splitData(){
-        
-        //if statements need to have changed values for data
-        int r = random.nextInt(numOfPlayers);
-        int r2 = 0;
 
-        if(r <= 22){
-            r2 = random.nextInt(2);
-            zeroToOne++;
-        } else if (r > 22 && r <= 63){
-            int result = random.nextInt(6);
-		        while(result < 2 || result >= 6) {
-			        result = random.nextInt(6);
-		        }
-            r2 = result;
-            twoToFive++;
-            
-        } else {
-            int result = random.nextInt(9);
-		        while(result < 6 || result >= 9) {
-			        result = random.nextInt(9);
-		        }
-            r2 = result;
-            sixToNine++;
+        //from Pseudocode Dan O. wrote in class
+        // 0.0 - 0.22: 0-1 Strikes
+        // 0.22 - 0.90: 2-5 Strikes
+        // 0.9 - 1.0: 6-9 Strikes
+
+        
+
+        int r = random.nextInt(0,101); //Simulates a random number from 0-100 for determining how many strikes
+        int r2 = 0; //This variable will determine the actual number of stikes onces the proper range is determined
+
+        if(r <= 43){ 
+            r2 = random.nextInt(0,2);
+        }
+         else if (r > 43 && r <= 87){
+            r2 = random.nextInt(2,4);
+        }  
+        else if (r > 87 && r <= 99){
+             r2 = random.nextInt(4,6);
 
         }
-        
-        return (r2);
+         else {
+            r2 = random.nextInt(6,9);
+        }
+        totalSplit += r2;
+        return (r2); //Returns the value of number of strikes for a particular bowler in 10 frames
     }
-
     public int missData(){
 
-        //if statements need to have changed values for data
-        int r = random.nextInt(numOfPlayers);
-        int r2 = 0;
+        //from Pseudocode Dan O. wrote in class
+        // 0.0 - 0.22: 0-1 Strikes
+        // 0.22 - 0.90: 2-5 Strikes
+        // 0.9 - 1.0: 6-9 Strikes
 
-        if(r <= 22){
-            r2 = random.nextInt(2);
-            zeroToOne++;
-        } else if (r > 22 && r <= 63){
-            int result = random.nextInt(6);
-		        while(result < 2 || result >= 6) {
-			        result = random.nextInt(6);
-		        }
-            r2 = result;
-            twoToFive++;
-            
-        } else {
-            int result = random.nextInt(9);
-		        while(result < 6 || result >= 9) {
-			        result = random.nextInt(9);
-		        }
-            r2 = result;
-            sixToNine++;
+        
+
+        int r = random.nextInt(0,101); //Simulates a random number from 0-100 for determining how many strikes
+        int r2 = 0; //This variable will determine the actual number of stikes onces the proper range is determined
+
+        if(r <= 41){ 
+            r2 = random.nextInt(0,2);
+        }
+         else if (r > 41 && r <= 86){
+            r2 = random.nextInt(2,4);
+        }  
+        else if (r > 86 && r <= 99){
+             r2 = random.nextInt(4,6);
+
 
         }
-        
-        return (r2);
+         else {
+            r2 = random.nextInt(6,9);
+        }
+        totalMiss += r2;
+        return (r2); //Returns the value of number of strikes for a particular bowler in 10 frames
     }
 
     public int noSpareData(){
-        
-        //if statements need to have changed values for data
-        int r = random.nextInt(numOfPlayers);
-        int r2 = 0;
 
-        if(r <= 22){
-            r2 = random.nextInt(2);
-            zeroToOne++;
-        } else if (r > 22 && r <= 63){
-            int result = random.nextInt(6);
-		        while(result < 2 || result >= 6) {
-			        result = random.nextInt(6);
-		        }
-            r2 = result;
-            twoToFive++;
-            
-        } else {
-            int result = random.nextInt(9);
-		        while(result < 6 || result >= 9) {
-			        result = random.nextInt(9);
-		        }
-            r2 = result;
-            sixToNine++;
+        //from Pseudocode Dan O. wrote in class
+        // 0.0 - 0.22: 0-1 Strikes
+        // 0.22 - 0.90: 2-5 Strikes
+        // 0.9 - 1.0: 6-9 Strikes
+
+        
+
+        int r = random.nextInt(0,101); //Simulates a random number from 0-100 for determining how many strikes
+        int r2 = 0; //This variable will determine the actual number of stikes onces the proper range is determined
+
+        if(r <= 12){ 
+            r2 = random.nextInt(0,2);
+        }
+         else if (r > 12 && r <= 50){
+            r2 = random.nextInt(2,4);
+        }  
+        else if (r > 50 && r <= 83){
+             r2 = random.nextInt(4,6);
 
         }
-        
-        return (r2);
+         else {
+            r2 = random.nextInt(6,9);
+        }
+        totalNoSpares += r2;
+        return (r2); //Returns the value of number of strikes for a particular bowler in 10 frames
     }
 
 
@@ -251,7 +240,8 @@ public class BowlingSim {
     }
 
     public String strikeDebug(){
-        return "Strike Distributions:\n" + zeroToOne + ": 1-2 Strikes\n" + twoToFive + ": 2-5 Strikes\n" + sixToNine + ": 6-9 Strikes\n\n";
+        return "Total Statistic Distributions:\n" + totalStrikes + ": # of Strikes\n" + totalSpares + ": # of Spares\n" + totalGB + ": # of Gutter Balls\n" 
+        + totalSplit + ": # of Splits\n" + totalMiss + ": # of Total Misses\n" + + totalNoSpares + ": # of Frames with no strike or spare\n\n" ;
     }
 
     public String toString(){
